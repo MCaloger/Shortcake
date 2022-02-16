ac1b2f493ec43c23b0ec31c41e5d4d15f84a6a03
Matthew Caloger <mattcaloger@gmail.com>
2022-02-15 23:59:26 -0500

    Addedtests

--- Services/UrlSanitizer.js
+++ Services/UrlSanitizer.js
@@ -1,10 +1,18 @@
-const sanitizeUrl = require("@braintree/sanitize-url").sanitizeUrl
-
 class UrlSanitizer {
 
+
     sanitize(input) {
-        console.log(sanitizeUrl)
-        return sanitizeUrl(input.toString());
+        try {
+            let url = new URL(input)
+
+            if(url.protocol === "http:" || url.protocol === "https:") {
+                return url
+            } else {
+                throw new Error("Invalid Url protocol, must he http or https.")
+            }
+        } catch (err) {
+            throw new Error("Invalid Url.");
+        }
     }
 }
 
--- Services/UrlSanitizer.test.js
+++ Services/UrlSanitizer.test.js
@@ -3,9 +3,24 @@ const UrlSanitizer = require('./UrlSanitizer')
 require('dotenv').config()
 
 
-describe('Url sanitizer works', () => {
-    const string = '!@#$%^&*()////<script>alert("hello");</script>'
-    it('should test that string is sanitized', () => {
-        expect(UrlSanitizer.sanitize(string)).toBe(string);
+describe('Url sanitizer', () => {
+    it('should return the Url if it validates', () => {
+        const path = "https://caloger.com/";
+        expect(
+            UrlSanitizer.sanitize(path)
+        ).toBe(path);
+    })
+
+    it('should test that error is thrown on invalid URL', () => {
+        expect(() => {
+            UrlSanitizer.sanitize('!@#$%^&*()////<script>alert("hello");</script>')
+        }).toThrow('Invalid Url.')
+    })
+
+
+    it('should test that error is thrown on invalid URL protocol', () => {
+        expect(() => {
+            UrlSanitizer.sanitize("javascript:void")
+        }).toThrow('Invalid Url.')
     })
 })
